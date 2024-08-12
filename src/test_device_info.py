edfa0491116e52cc2ebee1210ffab9652e84ebe4
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-08-12 22:15:53 +0900

    Added: test_device_info.py

---
+++ src/test_device_info.py
@@ -0,0 +1,45 @@
+'''
+device_info.py
+
+This program obtains sound device info, and stores the name of the device and the number of channels as type "dict".
+This program does not display anything.
+'''
+
+import sounddevice as sd
+import numpy as np
+
+def device_info():
+    input_info = {}
+    output_info = {}
+    driver_info = {'name':[], 'in_latency':[], 'out_latency':[]}
+    for hostapi in sd.query_hostapis():
+        hostapi_name = hostapi["name"]
+
+        # Device Info
+        in_ltc = []
+        out_ltc = []
+        input_info[hostapi_name]    = {"id":[], "name": [], "ch":[]}
+        output_info[hostapi_name]   = {"id":[], "name": [], "ch":[]}
+        for device_number in hostapi["devices"]:
+            device = sd.query_devices(device=device_number)
+            max_in_ch  = device["max_input_channels"]
+            max_out_ch = device["max_output_channels"]
+
+            if max_in_ch > 0:
+                input_info[hostapi_name]["name"].append(device['name'])
+                input_info[hostapi_name]["ch"].append(max_in_ch)
+                input_info[hostapi_name]["id"].append(device_number)
+                in_ltc.append(device["default_low_input_latency"])
+
+            if max_out_ch > 0:
+                output_info[hostapi_name]["name"].append(device['name'])
+                output_info[hostapi_name]["ch"].append(max_out_ch)
+                output_info[hostapi_name]["id"].append(device_number)
+                out_ltc.append(device["default_low_output_latency"])
+
+        # Driver Info
+        driver_info["name"].append(hostapi_name)
+        driver_info["in_latency"].append(np.mean(np.array(in_ltc)))
+        driver_info["out_latency"].append(np.mean(np.array(out_ltc)))
+
+    return driver_info, input_info, output_info
